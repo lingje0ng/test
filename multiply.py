5a1a4342387d0343f68f5a0a24992f24f936dcca
lingje0ng <kjh1306@naver.com>
2022-05-24 21:46:25 +0900

    'test'

---
+++ multiply.py
@@ -0,0 +1,11 @@
+x = int(input('정수 입력: '))
+y = int(input('정수 입력: '))
+
+num = 1
+
+for i in range(1,y+1):
+    num = num*y:
+    if num != x**y:
+        print("{}*".format(x),end='')
+    elif num == x**y:
+        print("{} = {}".format(x,x**y))
